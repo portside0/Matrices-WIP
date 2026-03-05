08f03446098642678d6cd2e735323cd91b8bb034
portside0 <studies0and0observations@gmail.com>
2026-03-05 10:22:40 +0530

    MOAR FUNCTIONS
    Added new matrix operations including transpose, gaussian elimination, and determinant calculation.

--- matrix.h
+++ matrix.h
@@ -31,6 +31,10 @@ matrix matrix_multiply(matrix* m1, matrix* m2);
 
 void matrix_scale(matrix* m, float k);
 
+void multiply_row(matrix* m, float k, int r);
+
+void multiply_col(matrix* m, float k, int c);
+
 void swap_rows(matrix* m, int r1, int r2);
 
 void swap_cols(matrix* m,int c1,int c2);
@@ -38,4 +42,11 @@ void swap_cols(matrix* m,int c1,int c2);
 void add_rows(matrix* m, float k, int r1, int r2);
 
 void add_cols(matrix* m, float k, int c1, int c2);
+
+matrix transpose(matrix* m);
+
+void gaussian(matrix* m);
+
+float det(matrix* m);
+
 #endif
